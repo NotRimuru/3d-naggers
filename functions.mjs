c3b788bb56f958383e4214b26cddc63b2fb10bb8
Skela <144936854+NotRimuru@users.noreply.github.com>
2025-03-28 09:14:09 +0100

    Update and rename functions.js to functions.mjs

--- functions.mjs
+++ functions.mjs
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import * as ENTITIES from "/entities.js";
+import * as ENTITIES from "/entities.mjs";
 
 export function damage() {
     const damage = ENTITIES.player.damage;
